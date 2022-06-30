6ffe15ed3d50cc7719b3ce68758203c24c733dae
grbrjlz <39337013+grbrjlz@users.noreply.github.com>
2022-06-30 10:49:00 +0200

    schrumpfen wachsen angepasst

--- Assets/Scripts/HsRm/JuLu/Core/Commands/GrowShrink/ActionOnTrigger.cs
+++ Assets/Scripts/HsRm/JuLu/Core/Commands/GrowShrink/ActionOnTrigger.cs
@@ -9,20 +9,20 @@ public class ActionOnTrigger : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("grow"))
+        if (other.gameObject.CompareTag("grow"))  
         {
             GetComponent<GrowOrShrink>().grow();
-            other.tag = "Untagged";
+            //other.tag = "Untagged";
         }
         if (other.gameObject.CompareTag("shrink"))
         {
             GetComponent<GrowOrShrink>().shrink();
-            other.tag = "Untagged";
+            //other.tag = "Untagged";
         }
         if (other.gameObject.CompareTag("normal_size"))
         {
             GetComponent<GrowOrShrink>().originalSize();
-            other.tag = "Untagged";
+            //other.tag = "Untagged";
         }
     }
 }
